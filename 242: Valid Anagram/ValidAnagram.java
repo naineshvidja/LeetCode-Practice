61a3eabe212d76c715d86ea2d4545ad8df2e16cf
naineshvidja <nainesh.vidja@gmail.com>
2024-06-08 13:01:42 +0530

    Update ValidAnagram.java

--- 242: Valid Anagram/ValidAnagram.java
+++ 242: Valid Anagram/ValidAnagram.java
@@ -3,18 +3,20 @@ class ValidAnagram {
     public boolean isAnagram(String s, String t) {
         char[] ss = s.toCharArray();
         char[] tt = t.toCharArray();
-
+    
         Arrays.sort(ss);
         Arrays.sort(tt);
-
+    
         return Arrays.equals(ss,tt);
     }
-  public static void main(String[] args)
-  {
-    boolean answer;
-    String s="anagram";
-    String t="nagaram";
-    answer=ValidAnagram.isAnagram(s,t);
-
-    System.out.println(answer);
+    public static void main(String[] args)
+    {
+        ValidAnagram va= new ValidAnagram();
+        boolean answer;
+        String s="anagram";
+        String t="nagaram";
+        answer=va.isAnagram(s,t);
+        
+        System.out.println(answer);
+    }
 }
